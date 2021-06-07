e765d1f1089ed1aace0bc527d2a4afe9704eecb5
Jonas hofmann <Jonash871j@gmail.com>
2021-06-07 13:17:49 +0200

    Added enums.

---
+++ H2-ZAPBeachCamping/Enum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZAPBeachCampingLib
+{ 
+    public enum SpotType
+    {
+        TentSite = 1,
+        CampingSite = 2,
+        HutSite = 3,
+    };
+
+    public enum HutType
+    {
+        Default = 1,
+        Luxury = 2,
+    }
+    public enum CampingType
+    {
+        Small = 1,
+        Large = 2,
+        SeasonLarge = 3
+    }
+    public enum CustomerType
+    {
+        Adult = 1,
+        Child = 2,
+        Dog = 3,
+    }
+}
