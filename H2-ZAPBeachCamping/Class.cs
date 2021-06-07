c7d4d8c69f2360108e12523e389537b407ba36e2
Jonas hofmann <Jonash871j@gmail.com>
2021-06-07 09:34:08 +0200

    Add project files.

---
+++ H2-ZAPBeachCamping/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZAPBeachCampingLib
+{ 
+    public class Class
+    {
+        public int Dummy { get; set; }
+    }
+}
